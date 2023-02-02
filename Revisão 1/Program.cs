60e00a77db27e855647da115bd8fd041a1bf9d97
Guilherme Henrique <94801953+GuilhermeRocha13@users.noreply.github.com>
2023-02-02 12:57:41 -0300

    Add files via upload

---
+++ Revisão 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace atv1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Func(666));
+        }
+
+        static double Func(double caralho)
+        {
+
+            double volume = (4 * 3.14 * Math.Pow(caralho, 3) / 3);
+
+
+            return volume;
+
+        }
+    }
+}
