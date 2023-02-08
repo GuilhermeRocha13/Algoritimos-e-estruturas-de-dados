7f93d4d4902b5769c0c9dd8d19dd9f0aef207cf4
Guilherme Henrique <94801953+GuilhermeRocha13@users.noreply.github.com>
2023-02-08 17:32:04 -0300

    Update Atv4.cs

--- Revisão 1/Atv4.cs
+++ Revisão 1/Atv4.cs
@@ -6,13 +6,13 @@ namespace revisao
     {
         static void Main(string[] args)
         {
-            int[,] vendas = new int[12, 4];
+            int[,] vendas = new int[12, 4]; // iniciando matriz
 
             for (int i = 0; i < 12; i++)
             {
                 Console.WriteLine("vendas do mês " + (i + 1) + ":");
                 for (int j = 0; j < 4; j++)
-                {
+                {                                                           //preenchendo 4 semanas para cada mês
                     Console.Write("Semana " + (j + 1) + ": ");
                     vendas[i, j] = int.Parse(Console.ReadLine());
                 }
@@ -24,23 +24,23 @@ namespace revisao
                 {
                     totaisMensais[i] += vendas[i, j];
                 }
-                Console.WriteLine("vendido no mês " + (i + 1) + ": " + totaisMensais[i]);
+                Console.WriteLine("vendido no mês " + (i + 1) + ": " + totaisMensais[i]); //somando as posições da matriz 
             }
 
-            // Calcular total vendido no ano
+
             int totalAnual = 0;
             for (int i = 0; i < 12; i++)
             {
                 totalAnual += totaisMensais[i];
             }
-            Console.WriteLine(" vendido no ano: " + totalAnual);
+            Console.WriteLine(" vendido no ano: " + totalAnual);              // Calcular total vendido no ano
 
-            // Encontrar a melhor semana para vender carros
+           
             int[] totaisSemanais = new int[4];
             for (int i = 0; i < 4; i++)
             {
                 for (int j = 0; j < 12; j++)
-                {
+                {                                                    // Encontrar a melhor semana para vender carros
                     totaisSemanais[i] += vendas[j, i];
                 }
             }
@@ -50,7 +50,7 @@ namespace revisao
             {
                 if (totaisSemanais[i] > totaisSemanais[melhorSemana])
                 {
-                    melhorSemana = i;
+                    melhorSemana = i;                                               //verificando qaul a melhor semana para venda
                 }
             }
 
